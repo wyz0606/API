acb2bdb2de96e0affe4ecc97ca47c37165dff4fd
Yanzhe Wu <wuy420@newschool.edu>
2019-04-17 17:08:30 -0400

    change to use cat API

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 
 
 function changeBackgroundImage(){
-	$.ajax({
-			url: "https://api.unsplash.com/photos/random",
-			method: "GET",
-			dataType: "json",
-			headers: {
-   				"Authorization": "Client-ID dff55246a193c9d14d513c0cdde1ac11958055b2533c08df372c298e9dd8720d",
-  			},
-  			success: function(data){
-  				var imgurl = data.urls.full
-  				$("#back").attr("src", imgurl);
-  			}
-	})
+	$.getJSON("https://api.thecatapi.com/v1/images/search",function(data){
+		console.log(data);
+		var url = data[0].url;
+		console.log(url);
+		$("#back").attr("src",url);
+	});
 }
 
 
